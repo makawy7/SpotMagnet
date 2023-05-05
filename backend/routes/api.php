52d4c29cf0132921abbebe69131ffe00449ac179
abdallah <abdallahelmekawy6@gmail.com>
2023-05-06 00:09:48 +0300

    profile controller and routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('auth/profile', [Auth\ProfileController::class, 'show']);
+    Route::put('auth/profile', [Auth\ProfileController::class, 'update']);
+});
