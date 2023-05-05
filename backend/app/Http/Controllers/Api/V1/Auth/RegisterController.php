250526ed117ea44daf96772978f943247d7d09fb
abdallah <abdallahelmekawy6@gmail.com>
2023-05-05 23:51:53 +0300

    register controller

--- backend/app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        return response()->json([
-            'message' => 'Register success',
+        $request->validate([
+            'name' => ['required', 'string', 'max:255', 'min:3'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', 'string', 'min:8'],
         ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        $device = substr($request->userAgent() ?? 'unknown', 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ], Response::HTTP_CREATED);
     }
 }
