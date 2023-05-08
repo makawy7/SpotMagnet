641878de770268e3bcef4af8e18d47352095a500
abdallah <abdallahelmekawy6@gmail.com>
2023-05-08 23:20:07 +0300

    Vehicle resource

--- backend/app/Http/Resources/VehicleResource.php
+++ backend/app/Http/Resources/VehicleResource.php
@@ -14,6 +14,9 @@ class VehicleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'plate_number' => $this->plate_number
+        ];
     }
 }
