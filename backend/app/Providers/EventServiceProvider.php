d5cef6dc69e4e9e433d47ed1e1bca78bf48d49eb
abdallah <abdallahelmekawy6@gmail.com>
2023-05-12 23:38:53 +0300

    adding parking user_id and start time with parkingobserver class

--- backend/app/Providers/EventServiceProvider.php
+++ backend/app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Observers\ParkingObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +19,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+    ];
+
+
+    protected $observers = [
+        Vehicle::class => [VehicleObserver::class],
+        Parking::class => [ParkingObserver::class],
     ];
 
     /**
