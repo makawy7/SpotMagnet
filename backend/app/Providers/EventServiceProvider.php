8773ec1a56a8a9ea6f1b2d7d05e3cb33d8302928
abdallah <abdallahelmekawy6@gmail.com>
2023-05-13 01:46:12 +0300

    fix importing classes in EventServiceProvider

--- backend/app/Providers/EventServiceProvider.php
+++ backend/app/Providers/EventServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Parking;
+use App\Models\Vehicle;
 use App\Observers\ParkingObserver;
+use App\Observers\VehicleObserver;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
